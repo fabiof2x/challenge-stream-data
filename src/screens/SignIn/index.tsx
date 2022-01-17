93c766e40b3b38f016e2aeacf45106785d862c57
fabiof2x <fabioar@gmail.com>
2022-01-17 19:17:32 -0300

    fluxo de SignIn

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -8,17 +8,17 @@ import { useAuth } from '../../hooks/useAuth';
 import LoginBannerImg from '../../assets/images/login.svg';
 import LogoImg from '../../assets/images/logo.svg';
 
-import { 
+import {
   Container,
   Content,
-  LoginBanner, 
-  LoginInfo, 
-  Header, 
-  Partner, 
-  Description, 
+  LoginBanner,
+  LoginInfo,
+  Header,
+  Partner,
+  Description,
   SignInButton,
   SignInButtonIcon,
-  SignInButtonText 
+  SignInButtonText
 } from './styles';
 
 export function SignIn() {
@@ -26,8 +26,15 @@ export function SignIn() {
   const theme = useTheme();
 
   // creates a function to handle sign in
-    // try to call and wait signIn
-    // if fails, display an Alert with the title "Erro SignIn" and message "Ocorreu um erro ao tentar logar no app"
+  async function handleSignIn() {
+    try {
+      // try to call and wait signIn
+      await signIn();
+    } catch (error) {
+      // if fails, display an Alert with the title "Erro SignIn" and message "Ocorreu um erro ao tentar logar no app"
+      Alert.alert('Erro SignIn', 'Ocorreu um erro ao tentar logar no app')
+    }
+  }
 
   return (
     <Container
@@ -61,23 +68,40 @@ export function SignIn() {
             o mundo da Twitch
           </Description>
 
-          {/* <SignInButton onPress={}>
+          <SignInButton onPress={handleSignIn}>
             <SignInButtonIcon>
-              Verify if isLoggingIn is true
-              If it is, show an ActivityIndicator
-              Otherwise, show Fontisto's twitch icon
+              {
+                // Verify if isLoggingIn is true
+                isLoggingIn
+                  // If it is, show an ActivityIndicator
+                  ? <ActivityIndicator
+                    size={20}
+                    color={theme.colors.white}
+                  />
+                  // Otherwise, show Fontisto's twitch icon
+                  : <Fontisto
+                    name="twitch"
+                    size={20}
+                    color={theme.colors.white}
+                  />
+              }
             </SignInButtonIcon>
 
             <SignInButtonText>
-              Verify if isLoggingIn is true
-              If it is, show "Entrando..."
-              Otherwise, show "Entrar com Twitch"
+              {
+                isLoggingIn
+                  //Verify if isLoggingIn is true
+                  ? 'Entrando...'
+                  //If it is, show "Entrando..."
+                  : 'Entrar com Twitch'
+                //Otherwise, show "Entrar com Twitch"
+              }
             </SignInButtonText>
-          </SignInButton> */}
+          </SignInButton>
         </LoginInfo>
       </Content>
 
-      <Modal 
+      <Modal
         animationType="fade"
         visible={isLoggingIn}
         statusBarTranslucent
